41c2ca497a26219032df1a03ad52f9a81e4f1bc2
Coralsnake Kaka <carenmoore34@gmail.com>
2026-09-11 16:49:53 +0800

    完成第二步设计以及跟进了目录进度，目前可以提取第一页标题

--- spider_db/README.md
+++ spider_db/README.md
@@ -4,7 +4,8 @@
 这是我学习Python爬虫的第一个练手项目，题目来自学习圈，有参考bilibili热搜榜爬虫项目。
 
 ## 当前进度
-- 可以成功请求豆瓣首页并打印HTML
+- [x]第一版：可以成功请求豆瓣首页并打印HTML
+- [x]第二版：可以提取出第一页电影名称
 
 ## 运行
 pip install requests beautifulsoup
--- spider_db/experiment1.py
+++ spider_db/experiment1.py
@@ -1,9 +1,19 @@
 import requests
+from bs4 import BeautifulSoup
 
 url = "https://movie.douban.com/top250"
 headers = {
     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/152.0.0.0 Safari/537.36 Edg/152.0.0.0"
 }
 
-echo = requests.get(url, headers=headers)
-print(echo.text)
+response = requests.get(url, headers=headers)
+response.encoding = "utf-8"
+
+soup = BeautifulSoup(response.text, "html.parser")
+movie_items = soup.find_all("div", class_="item")
+
+for item in movie_items:
+    title_span = item.find("span", class_="title")
+    if title_span:
+        print(title_span.text)
+        
