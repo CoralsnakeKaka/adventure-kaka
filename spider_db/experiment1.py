1bc66c9d8d5f8d1b42b0a3c7234ac4cb7db3dff8
Coralsnake Kaka <carenmoore34@gmail.com>
2026-09-12 22:32:01 +0800

    可以做到提取整个榜单电影名称

--- spider_db/experiment1.py
+++ spider_db/experiment1.py
@@ -1,19 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
+import time
 
 url = "https://movie.douban.com/top250"
 headers = {
     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/152.0.0.0 Safari/537.36 Edg/152.0.0.0"
 }
 
-response = requests.get(url, headers=headers)
-response.encoding = "utf-8"
+total = []
+for page in range(0, 10):
+    page_url = f"{url}?start={page * 25}"
+    response = requests.get(page_url, headers=headers)
+    response.encoding = "utf-8"
 
-soup = BeautifulSoup(response.text, "html.parser")
-movie_items = soup.find_all("div", class_="item")
+    soup = BeautifulSoup(response.text, "html.parser")
+    movie_items = soup.find_all("div", class_="item")
 
-for item in movie_items:
-    title_span = item.find("span", class_="title")
-    if title_span:
-        print(title_span.text)
-        
+    for item in movie_items:
+        title_span = item.find("span", class_="title")
+        if title_span:
+            total.append(title_span.text)
+
+    time.sleep(2)
+    print("page",page+1,"state",response.status_code)
+    print("Total movies found:", len(total))
+
+print(total)
